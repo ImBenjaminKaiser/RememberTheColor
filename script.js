c50d72d2c4439aa076053c3b57462ac8605358f6
Benjamin Kaiser <bnkaiser04@gmail.com>
2025-12-29 23:52:29 -0500

    cont get color variants function

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
+// https://stackoverflow.com/a/24152886
+function randomInteger(rangeArray) {
+  return Math.floor(Math.random() * (rangeArray[1] - rangeArray[0] + 1)) + rangeArray[0];
+}
 
-function getColorVariants(inputColor, hueVariance, SaturationVariance, LuminanceVariance) {
-    satReplaced = inputColor.replace(/(?<=, )[^,]+(?=%,)/gm, SaturationVariance)
-    lumReplaced = satReplaced.replace(/(?<=%, )[^,]+(?=%)/gm, LuminanceVariance)
-    hueReplaced = lumReplaced.replace(/(?<=hsl\()[^,]+(?=,)/gm, hueVariance)
 
-    console.log(hueReplaced)
+function getColorVariants(inputColor, hueVariance, satVariance, lumVariance) {
+
+    colorSquares = document.getElementsByClassName("colorGridSquare")
+    console.log(colorSquares)
+    computedColorValuesArray = []
+
+    let inputHue = /(?<=hsl\()[^,]+(?=,)/gm.exec(inputColor) // Get Hue value from hsl input
+    let inputSat = /(?<=, )[^,]+(?=%,)/gm.exec(inputColor) // Get Saturation value from hsl input
+    let inputLum = /(?<=%, )[^,]+(?=%)/gm.exec(inputColor) // Get Luminance value from hsl input
+
+    // Regex matching method returns object with unneeded extra info, so get first value of the object,
+    // Which is whatever the regex matched
+
+    inputHue = Object.values(inputHue)[0]
+    inputSat = Object.values(inputSat)[0]
+    inputLum = Object.values(inputLum)[0]
+
+    console.log(inputColor, inputHue, inputSat, inputLum)
+
+    let hueRandomNumberRange = [Number(inputHue) + Number(hueVariance), Number(inputHue) - Number(hueVariance)]
+    let satRandomNumberRange = [Number(inputSat) + Number(satVariance), Number(inputSat) - Number(satVariance)]
+    let lumRandomNumberRange = [Number(inputLum) + Number(lumVariance), Number(inputLum) - Number(lumVariance)]
+
+    console.log(hueRandomNumberRange, satRandomNumberRange, lumRandomNumberRange)
+
+    let correctColorIndex = randomInteger([1, 9])
+
+    console.log(correctColorIndex)
+
+    for (let x = 1; x <= 9; x++) {
+        if (correctColorIndex-1 == x) {
+            console.log("loop index matched correct square index")
+            correctSquare = console.log(colorSquares[x-1])
+            correctSquare.style.setProperty("backgroundColor", inputColor)
+        }
+
+        else {
+
+            let adjustedHueValue = randomInteger(hueRandomNumberRange)
+            let adjustedSatValue = randomInteger(satRandomNumberRange)
+            let adjustedLumValue = randomInteger(lumRandomNumberRange)
+
+            satReplaced = inputColor.replace(/(?<=, )[^,]+(?=%,)/gm, satVariance)
+            lumReplaced = satReplaced.replace(/(?<=%, )[^,]+(?=%)/gm, lumVariance)
+            hueReplaced = lumReplaced.replace(/(?<=hsl\()[^,]+(?=,)/gm, hueVariance)
+            computedColorValuesArray.push(hueReplaced)
+        }
+    }
+    // console.log(hueReplaced)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -22,6 +70,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     if (page == "gamePage") {
-        getColorVariants("hsl(100, 100%, 50%)", "replace", "5", "5")
+        getColorVariants("hsl(100, 100%, 50%)", 5, 5, 5)
     }
 })
